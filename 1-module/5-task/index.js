e1d93be76c4f64b54dcb0dfc1f15bbb60e6089fb
Artem Gradovskiy <russia.toxin@gmail.com>
2024-04-29 13:11:58 +0300

    решение 3-5 задач

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,13 @@
 function truncate(str, maxlength) {
-  // ваш код...
-}
+    // return str.length <= maxlength ? str : str.slice(0, maxlength - 1) + "…"; // вариант 1 решение без if
+    if (str.length <= maxlength) { // вариант 2 с if
+      return str
+    }
+    else {
+      return str.slice(0, maxlength - 1) + "…"
+    }
+  }
+
+alert(truncate('Вот, что мне хотелось бы сказать на эту тему:', 20)) === 'Вот, что мне хотело…';
+alert(truncate('New york Rangers', 16)) // === 'Всем привет!';
+alert(truncate('New york Rangers', 10))
