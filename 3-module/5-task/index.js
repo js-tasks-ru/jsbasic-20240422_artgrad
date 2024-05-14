e4715568d095c9dd3ff0689ba27f892deb42e9f2
Artem Gradovskiy <russia.toxin@gmail.com>
2024-05-15 01:37:24 +0300

    решение 3 модуля

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,10 @@
 function getMinMax(str) {
-  // ваш код...
+  let ourNumbers = str
+        .split(' ')
+        .filter(function (item) { return Number(item) });
+
+      return {
+        min: Math.min(...ourNumbers),
+        max: Math.max(...ourNumbers),
+      };
 }
